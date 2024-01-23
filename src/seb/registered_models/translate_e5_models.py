e1cefd919fa69b9c9edf36eb960e258613322d2b
Márton Kardos <power.up1163@gmail.com>
2024-01-23 08:59:52 +0100

    Translate now returns a single string instead of a list

--- src/seb/registered_models/translate_e5_models.py
+++ src/seb/registered_models/translate_e5_models.py
@@ -26,7 +26,9 @@ class TranslateE5Model(seb.Encoder):
         gen_tokens = self.trans_model.generate(
             **encoded_sent, forced_bos_token_id=self.trans_tokenizer.get_lang_id("en")
         )
-        return self.trans_tokenizer.batch_decode(gen_tokens, skip_special_tokens=True)
+        return self.trans_tokenizer.batch_decode(gen_tokens, skip_special_tokens=True)[
+            0
+        ]
 
     def encode(
         self,

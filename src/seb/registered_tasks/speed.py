cfccbdf3aa32e8b73e4d89ac924ac1eefac64986
Márton Kardos <power.up1163@gmail.com>
2024-01-23 13:05:22 +0100

    Fixed import error in speed task

--- src/seb/registered_tasks/speed.py
+++ src/seb/registered_tasks/speed.py
@@ -9,10 +9,10 @@ import numpy as np
 import psutil
 import torch
 
+from seb.interfaces.language import languages_in_seb
 from seb.interfaces.model import EmbeddingModel
-from seb.interfaces.task import Task
+from seb.interfaces.task import DescriptiveDatasetStats, Task
 from seb.result_dataclasses import TaskResult
-from seb.types import DescriptiveDatasetStats, languages_in_seb
 
 logger = logging.getLogger(__name__)
 
@@ -45,7 +45,9 @@ class CPUSpeedTask(Task):
         dataset = self.load_dataset()
         lengths = np.array([len(x) for x in dataset])
         return DescriptiveDatasetStats(
-            mean_document_length=float(np.mean(lengths)), std_document_length=float(np.std(lengths)), num_documents=len(dataset)
+            mean_document_length=float(np.mean(lengths)),
+            std_document_length=float(np.std(lengths)),
+            num_documents=len(dataset),
         )
 
     def get_time_taken(self, model: EmbeddingModel) -> float:
@@ -67,10 +69,15 @@ class CPUSpeedTask(Task):
         if run_inference:
             time_taken = self.get_time_taken(model)
         else:
-            logger.warn(f"Could not run inference on {model.meta.name} on {self.device} as it does not have a 'to' method. Skipping")
+            logger.warn(
+                f"Could not run inference on {model.meta.name} on {self.device} as it does not have a 'to' method. Skipping"
+            )
             time_taken = np.nan
 
-        scores: dict[str, Union[str, float]] = {self.main_score: time_taken, **self.get_system_info()}
+        scores: dict[str, Union[str, float]] = {
+            self.main_score: time_taken,
+            **self.get_system_info(),
+        }
 
         return TaskResult(
             task_name=self.name,

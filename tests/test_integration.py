2f757121b8ef77fdf0120a6df3d3394dc19b5d1b
Márton Kardos <power.up1163@gmail.com>
2024-01-24 10:40:04 +0100

    Added Dummy task to integration test, remove all-MiniLM-L6

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,4 +1,38 @@
+from datetime import datetime
+
+import seb
 from seb.cli import run_benchmark_cli
+from seb.registries import tasks
+
+
+@tasks.register("test-encode-task")
+def create_test_encode_task() -> seb.Task:
+    class DummyTask(seb.Task):
+        name = "test-encode-task"
+        main_score = "a_metric"
+        description = "NA"
+        reference = "NA"
+        version = "NA"
+        languages = []  # noqa: RUF012
+        domain = []  # noqa: RUF012
+        task_type = "Classification"
+
+        def evaluate(self, model: seb.Encoder) -> seb.TaskResult:
+            model.encode(["a test sentence"], task=self)
+
+            return seb.TaskResult(
+                task_name="test-encode-task",
+                task_description="NA",
+                task_version="NA",
+                time_of_run=datetime.now(),
+                scores={"en": {"a_metric": 1.0}},
+                main_score="a_metric",
+            )
+
+        def get_descriptive_stats(self):
+            return {}
+
+    return DummyTask()
 
 
 def test_cli_integration():
@@ -6,10 +40,9 @@ def test_cli_integration():
     Cache is ignored so that the models are actually run.
     """
     models = [
-        "sentence-transformers/all-MiniLM-L6-v2",
+        "fasttext-cc-da-300",
         "intfloat/e5-small",
         "translate-e5-small",
-        "fasttext-cc-da-300",
     ]
-    tasks = ["LCC"]
+    tasks = ["test-encode-task"]
     run_benchmark_cli(models=models, tasks=tasks, ignore_cache=True)

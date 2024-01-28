c315d94d51795d3e62231997506e344731c6c98a
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2024-01-28 16:18:13 +0100

    feat: Added tv2nord retrieval dataset

--- src/seb/mteb_tasks/retrieval/__init__.py
+++ src/seb/mteb_tasks/retrieval/__init__.py
@@ -3,3 +3,4 @@ from .swefaq_retrieval import SweFaqRetrieval
 from .swedn_retrieval import SwednRetrieval
 from .swefaq_retrieval import SweFaqRetrieval
 from .snl_retrieval import SNLRetrieval
+from .t2nord_retrieval import TV2Nordretrieval

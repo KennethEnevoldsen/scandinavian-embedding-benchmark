617d6167479d2955f6034bd1fc6b3e4810eb591e
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2024-01-28 14:07:17 +0100

    fix: remove ingress from the SNL corpus as they almost always contain the headline

--- src/seb/mteb_tasks/retrieval/snl_retrieval.py
+++ src/seb/mteb_tasks/retrieval/snl_retrieval.py
@@ -6,9 +6,10 @@ Using two sample models (text-embedding-3-small, all-MiniLM-L6-v2) we get the fo
 96.07, 64.67
 
 if we then remove the ingress from the corpus we get:
-
+92.71, 51.61
 The reason why we might want to remove the ingress is that it almost always start with headline.
 
+As the scores are indeed slightly lower we will ignore the ingress as the task becomes too easy.
 """
 
 from typing import Any
@@ -66,30 +67,20 @@ class SNLRetrieval(AbsTaskRetrieval):
             ds: datasets.Dataset = self.dataset[split]  # type: ignore
             ds = ds.shuffle(seed=42)
 
-            max_samples = min(1024, len(ds))
-            ds = ds.select(range(max_samples))  # limit the dataset size to make sure the task does not take too long to run
             self.queries[split] = {}
             self.relevant_docs[split] = {}
             self.corpus[split] = {}
 
             headline = ds["headline"]
-            summary = ds["ingress"]
             article = ds["article"]
 
             n = 0
-            for headl, summ, art in zip(headline, summary, article):
+            for headl, art in zip(headline, article):
                 self.queries[split][str(n)] = headl
                 q_n = n
                 n += 1
-                if summ not in text2id:
-                    text2id[summ] = n
-                    # self.corpus[split][str(n)] = {"title": "", "text": summ}
-                    n += 1
                 if art not in text2id:
                     text2id[art] = n
                     self.corpus[split][str(n)] = {"title": "", "text": art}
                     n += 1
-                self.relevant_docs[split][str(q_n)] = {
-                    str(text2id[art]): 1,
-                    # str(text2id[summ]): 1
-                }  # only two correct matches
+                self.relevant_docs[split][str(q_n)] = {str(text2id[art]): 1}  # only one correct matches

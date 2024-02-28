8f055af89a5f7cc45318a8a153b9e18b18bec493
Kenneth Enevoldsen (UCloud) <kenneth.enevoldsen@cas.au.dk>
2024-02-28 20:10:06 +0100

    fix: Added full dataset using english prompt

--- src/seb/registered_models/e5_instruct_models.py
+++ src/seb/registered_models/e5_instruct_models.py
@@ -93,6 +93,125 @@ def task_to_instruction(task: Task) -> str:
     return ""
 
 
+
+
+def task_to_instruction_da(task: Task) -> str:
+    if task.task_type in ["STS"]:
+        return "Retrieve semantically similar text"
+    if task.task_type in ["Summarization"]:
+        return "Given a news summary, retrieve other semantically similar summaries"
+    if task.task_type in ["BitextMining"]:
+        task_name_to_instruct: dict[str, str] = {
+            "Bornholm Parallel": "Retrieve parallel sentences in Danish and Bornholmsk",
+            "Norwegian courts": "Retrieve parallel sentences in Norwegian Bokmål and Nynorsk",
+        }
+        default_instruction = "Retrieve parallel sentences."
+        return task_name_to_instruct.get(task.name, default_instruction)
+    if task.task_type in ["Classification"]:
+        task_name_to_instruct: dict[str, str] = {
+            "Angry Tweets": "Classify Danish tweets by sentiment. (positive, negative, neutral)",
+            "DKHate": "Classify Danish tweets based on offensiveness (offensive, not offensive)",
+            "Da Political Comments": "Classify Danish political comments for sentiment",
+            "DaLAJ": "Classify texts based on linguistic acceptability in Swedish",
+            "LCC": "Klassificér tekster baseret på deres sentiment",
+            "Language Identification": "Classify texts based on language",
+            "Massive Intent": "Given a user utterance as query, find the user intents",
+            "Massive Scenario": "Given a user utterance as query, find the user scenarios",
+            "NoReC": "Classify Norwegian reviews by sentiment",
+            "SweReC": "Classify Swedish reviews by sentiment",
+            "Norwegian parliament": "Classify parliament speeches in Norwegian based on political affiliation",
+            "ScaLA": "Classify passages in Scandinavian Languages based on linguistic acceptability",
+        }
+        default_instruction = "Classify user passages"
+        return task_name_to_instruct.get(task.name, default_instruction)
+    if task.task_type in ["Clustering"]:
+        task_name_to_instruct: dict[str, str] = {
+            "ArxivClusteringP2P": "Identify the main and secondary category of Arxiv papers based on the titles and abstracts",
+            "VG Clustering": "Identify the categories (e.g. sports) of given articles in Norwegian",
+            "SNL Clustering": "Identify categories in a Norwegian lexicon",
+            "SwednClustering": "Identify news categories in Swedish passages",
+        }
+        default_instruction = "Identify categories in user passages"
+        return task_name_to_instruct.get(task.name, default_instruction)
+    if task.task_type in ["Reranking"]:
+        return "Retrieve semantically similar passages."
+    if task.task_type in ["Retrieval"]:
+        task_name_to_instruct: dict[str, str] = {
+            "Twitterhjerne": "Retrieve answers to questions asked in Danish tweets",
+            "SwednRetrieval": "Given a Swedish news headline retrieve summaries or news articles",
+            "TV2Nord Retrieval": "Given a summary of a Danish news article retrieve the corresponding news article",
+            "DanFEVER": "Given a claim in Danish, retrieve documents that support the claim",
+            "SNL Retrieval": "Given a lexicon headline in Norwegian, retrieve its article",
+            "NorQuad": "Given a question in Norwegian, retrieve the answer from Wikipedia articles",
+            "SweFAQ": "Retrieve answers given questions in Swedish",
+            "ArguAna": "Given a claim, find documents that refute the claim",
+            "ClimateFEVER": "Given a claim about climate change, retrieve documents that support or refute the claim",
+        }
+        default_instruction = "Retrieve text based on user query."
+        return task_name_to_instruct.get(task.name, default_instruction)
+    return ""
+
+
+# def task_to_instruction_da(task: Task) -> str:
+#     if task.task_type in ["STS"]:
+#         return "Find semantisk lignende tekst"
+#     if task.task_type in ["Summarization"]:
+#         return "Givet et nyhedssammendrag, find andre semantisk lignende sammendrag"
+#     if task.task_type in ["BitextMining"]:
+#         task_name_to_instruct: dict[str, str] = {
+#             "Bornholm Parallel": "Find parallelle sætninger på dansk og bornholmsk",
+#             "Norwegian courts": "Find parallelle sætninger på norsk bokmål og nynorsk",
+#         }
+#         default_instruction = "Find parallelle sætninger."
+#         return task_name_to_instruct.get(task.name, default_instruction)
+#     if task.task_type in ["Classification"]:
+#         task_name_to_instruct: dict[str, str] = {
+#             "Angry Tweets": "Klassificér danske tweets efter sentiment. (positivt, negativt, neutralt)",
+#             "DKHate": "Klassificér danske tweets baseret på stødende indhold (stødende, ikke stødende)",
+#             "Da Political Comments": "Klassificér danske politiske kommentarer for sentiment",
+#             "DaLAJ": "Klassificér tekster baseret på sproglig acceptabilitet på svensk",
+#             "LCC": "Klassificér tekster baseret på sentiment",
+#             "Language Identification": "Klassificér tekster baseret på sprog",
+#             "Massive Intent": "Givet en brugerudtalelse som forespørgsel, find brugerens intentioner",
+#             "Massive Scenario": "Givet en brugerudtalelse som forespørgsel, find brugerens scenarier",
+#             "NoReC": "Klassificér norske anmeldelser efter sentiment",
+#             "SweReC": "Klassificér svenske anmeldelser efter sentiment",
+#             "Norwegian parliament": "Klassificér taler i det norske parlament baseret på politisk tilhørsforhold",
+#             "ScaLA": "Klassificér passager på skandinaviske sprog baseret på sproglig acceptabilitet",
+#         }
+#         default_instruction = "Klassificér brugerpassager"
+#         return task_name_to_instruct.get(task.name, default_instruction)
+#     if task.task_type in ["Clustering"]:
+#         task_name_to_instruct: dict[str, str] = {
+#             "ArxivClusteringP2P": "Identificer hoved- og sekundærkategorien af Arxiv-papirer baseret på titler og resuméer",
+#             "VG Clustering": "Identificer kategorierne (f.eks. sport) af givne artikler på norsk",
+#             "SNL Clustering": "Identificer kategorier i et norsk leksikon",
+#             "SwednClustering": "Identificer nyhedskategorier i svenske passager",
+#         }
+#         default_instruction = "Identificer kategorier i brugerpassager"
+#         return task_name_to_instruct.get(task.name, default_instruction)
+#     if task.task_type in ["Reranking"]:
+#         return "Find semantisk lignende passager."
+#     if task.task_type in ["Retrieval"]:
+#         task_name_to_instruct: dict[str, str] = {
+#             "Twitterhjerne": "Find svar på spørgsmål stillet i danske tweets",
+#             "SwednRetrieval": "Givet en svensk nyhedsoverskrift find resuméer eller nyhedsartikler",
+#             "TV2Nord Retrieval": "Givet et resumé af en dansk nyhedsartikel find den tilsvarende nyhedsartikel",
+#             "DanFEVER": "Givet en påstand på dansk, find dokumenter der understøtter påstanden",
+#             "SNL Retrieval": "Givet en leksikonoverskrift på norsk, find dens artikel",
+#             "NorQuad": "Givet et spørgsmål på norsk, find svaret i Wikipedia-artikler",
+#             "SweFAQ": "Find svar givet spørgsmål på svensk",
+#             "ArguAna": "Givet en påstand, find dokumenter der modbeviser påstanden",
+#             "ClimateFEVER": "Givet en påstand om klimaændringer, find dokumenter der understøtter eller modbeviser påstanden",
+#         }
+#         default_instruction = "Find tekst baseret på
+
+#  brugerforespørgsel."
+#         return task_name_to_instruct.get(task.name, default_instruction)
+#     return ""
+
+
+
 class E5Instruct(Encoder):
     def __init__(self, model_name: str, max_length: int, max_batch_size: Optional[int] = None, **kwargs: Any):
         logger.info("Started loading e5 instruct model")
@@ -100,6 +219,7 @@ class E5Instruct(Encoder):
         self.model = AutoModel.from_pretrained(model_name, **kwargs)
         self.max_length = max_length
         self.max_batch_size = max_batch_size
+        self.task_to_instruction = task_to_instruction
 
     def preprocess(self, sentences: Sequence[str], instruction: str, encode_type: EncodeTypes) -> BatchEncoding:
         if encode_type == "query":
@@ -136,7 +256,7 @@ class E5Instruct(Encoder):
             batch_size = self.max_batch_size
         batched_embeddings = []
         if task is not None:  # noqa
-            instruction = task_to_instruction(task)
+            instruction = self.task_to_instruction(task)
         else:
             instruction = ""
         for batch in tqdm(batched(sentences, batch_size)):
@@ -164,7 +284,7 @@ class E5Instruct(Encoder):
     
     def to(self, device: torch.device):
         self.model.to(device)
-        
+
 
 
 class E5Mistral(E5Instruct):
@@ -213,12 +333,14 @@ class E5PEFTInstruct(E5Mistral):
         repo_id = model_name
         config = PeftConfig.from_pretrained(repo_id)
 
-        base_model = AutoModel.from_pretrained(config.base_model_name_or_path, torch_dtype=torch.float16)
+        base_model = AutoModel.from_pretrained(config.base_model_name_or_path, torch_dtype=torch.float16, device_map="auto")
         self.model = PeftModel.from_pretrained(base_model, repo_id)
         self.tokenizer = AutoTokenizer.from_pretrained("intfloat/e5-mistral-7b-instruct")
         self.model = AutoModel.from_pretrained(model_name, **kwargs)
         self.max_length = max_length
         self.max_batch_size = max_batch_size
+
+        self.task_to_instruction = task_to_instruction_da
 
 
 @models.register("e5-mistral-7b-instruct")

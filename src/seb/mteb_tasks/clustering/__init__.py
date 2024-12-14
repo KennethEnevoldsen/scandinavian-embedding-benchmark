f52134428fd6c35f3e6f3819eecfb96cdf05ba5b
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2024-12-13 17:02:50 -0800

    fix import

--- src/seb/mteb_tasks/clustering/__init__.py
+++ src/seb/mteb_tasks/clustering/__init__.py
@@ -1,4 +1,3 @@
 from .swedn_clustering import SwednClustering
 from .vg_clustering import VGClustering
 from .snl_clustering import SNLClustering
-from .historical_danish_clustering import HistoricalDanishClustering
